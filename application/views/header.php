0ce9872a72247d6a62b883d961a1f76391361f1c
Dzywolf <luis.wolf.core@gmail.com>
2018-12-11 16:59:51 -0300

    Fixed some issues in chat

--- application/views/header.php
+++ application/views/header.php
@@ -22,10 +22,10 @@
     <script src="<?= base_url('core/js/jquery-3.3.1.min.js'); ?>"></script>
 
     <?php if($this->m_data->isLogged()) { ?>
-        <link type="text/css" rel="stylesheet" media="all" href="<?= base_url('assets/chat/css/chat/chat.css'); ?>" />
-        <link type="text/css" rel="stylesheet" media="all" href="<?= base_url('assets/chat/css/chat/screen.css'); ?>" />
-        <script type="text/javascript" src="<?= base_url('assets/chat/js/chat/jquery.js'); ?>"></script>
-        <script type="text/javascript" src="<?= base_url('assets/chat/js/chat/chat.js'); ?>"></script>
+        <link type="text/css" rel="stylesheet" media="all" href="<?= base_url('assets/chat/css/chat.css'); ?>" />
+        <link type="text/css" rel="stylesheet" media="all" href="<?= base_url('assets/chat/css/screen.css'); ?>" />
+        <script type="text/javascript" src="<?= base_url('assets/chat/js/jquery.js'); ?>"></script>
+        <script type="text/javascript" src="<?= base_url('assets/chat/js/chat.js'); ?>"></script>
     <?php } ?>
 
     <!-- Notify -->
